b25526666f62023780b0100584f462b11d16acbd
wln <w2252603132@gmail.com>
2025-07-05 18:06:37 +0800

    1. add csrf doc.

--- src/main/java/top/wei/oauth2/controller/CsrfController.java
+++ src/main/java/top/wei/oauth2/controller/CsrfController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
+ * CsrfController.
  */
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CsrfController {
 
+    /**
+     * return X-CSRF-TOKEN.
+     * @param token token
+     * @return csrf token
+     */
     @GetMapping("/csrf")
     public CsrfToken csrf(CsrfToken token) {
         return token;
