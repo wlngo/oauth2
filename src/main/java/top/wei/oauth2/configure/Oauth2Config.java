be2b6acaa457afa8c366282a8531521e0cb57cd6
wln <w2252603132@gmail.com>
2025-07-18 16:51:58 +0800

    1.  update consent.

--- src/main/java/top/wei/oauth2/configure/Oauth2Config.java
+++ src/main/java/top/wei/oauth2/configure/Oauth2Config.java
@@ -123,8 +123,7 @@ public class Oauth2Config {
                     new OAuth2AuthorizationServerConfigurer();
             //自定义授权页
             authorizationServerConfigurer.authorizationEndpoint(oAuth2AuthorizationEndpointConfigurer
-                    -> oAuth2AuthorizationEndpointConfigurer.consentPage("/#" +
-                            "/oauth2/consent"));
+                    -> oAuth2AuthorizationEndpointConfigurer.consentPage("/#/oauth2/consent"));
             //todo 撤销token端点 销毁redis key
 //        authorizationServerConfigurer.tokenRevocationEndpoint()
             //获取授权服务器端点
