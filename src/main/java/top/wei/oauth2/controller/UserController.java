80820f3378f26be308fa0be680383ee90d4fd7d1
wln <w2252603132@gmail.com>
2025-08-29 18:46:33 +0800

    1. update UserController.

--- src/main/java/top/wei/oauth2/controller/UserController.java
+++ src/main/java/top/wei/oauth2/controller/UserController.java
@@ -3,13 +3,7 @@ package top.wei.oauth2.controller;
 import com.github.pagehelper.PageInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.wei.oauth2.model.entity.User;
 import top.wei.oauth2.model.vo.UserInfoVo;
 import top.wei.oauth2.service.UserService;
@@ -20,7 +14,8 @@ import top.wei.oauth2.utils.RestBody;
 /**
  * UserController.
  */
-@RestController("/users")
+@RestController
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class UserController {
